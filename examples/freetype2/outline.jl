b73799b08ea694db33d3bc757f0f3a4763f451c4
WooKyoung Noh <wookay.noh@gmail.com>
2019-05-13 19:29:34 +0900

    poptart transform

--- examples/freetype2/outline.jl
+++ examples/freetype2/outline.jl
@@ -1,10 +1,10 @@
 using Allographs.FontEngines.FreeType2
 using Poptart.Desktop # Application
 using Poptart.Controls # Canvas
-using Poptart.Drawings # Line Curve stroke
+using Poptart.Drawings # Line Curve stroke translate scale
 using Colors # RGBA
 
-function put_glyph(f, canvas, commands)
+function put_glyph(transform, canvas, commands)
     thickness = 2
     strokeColor = RGBA(0.1, 0.7, 0.8, 0.9)
     lastPoint = (0, 0)
@@ -12,16 +12,16 @@ function put_glyph(f, canvas, commands)
         if cmd.type == 'M'
            lastPoint = cmd.point
         elseif cmd.type == 'L'
-           line = Line(points=f.([lastPoint, cmd.point]), thickness=thickness, color=strokeColor)
-           put!(canvas, stroke(line))
+           line = Line(points=[lastPoint, cmd.point], thickness=thickness, color=strokeColor)
+           put!(canvas, (stroke ∘ transform)(line))
            lastPoint = cmd.point
         elseif cmd.type == 'Q'
-           curve = Curve(startPoint=f(lastPoint), control1=f(cmd.control1), control2=f(cmd.control2), endPoint=f(cmd.endPoint), thickness=thickness, color=strokeColor)
-           put!(canvas, stroke(curve))
+           curve = Curve(startPoint=lastPoint, control1=cmd.control1, control2=cmd.control2, endPoint=cmd.endPoint, thickness=thickness, color=strokeColor)
+           put!(canvas, (stroke ∘ transform)(curve))
            lastPoint = cmd.endPoint
         elseif cmd.type == 'C'
-           curve = Curve(startPoint=f(lastPoint), control1=f(cmd.control), control2=f(cmd.endPoint), endPoint=f(cmd.endPoint), thickness=thickness, color=strokeColor)
-           put!(canvas, stroke(curve))
+           curve = Curve(startPoint=lastPoint, control1=cmd.control, control2=cmd.endPoint, endPoint=cmd.endPoint, thickness=thickness, color=strokeColor)
+           put!(canvas, (stroke ∘ transform)(curve))
            lastPoint = cmd.endPoint
         end
     end
@@ -32,9 +32,8 @@ width, height = 500, 500
 
 font_path = normpath(@__DIR__, "IropkeBatangM.ttf")
 commands = FreeType2.char_to_commands(font_path, '헐', 72)
-put_glyph(canvas, commands) do point
-    x, y = point
-    (x, height-y) ./ 7 .+ (20, 110)
+put_glyph(canvas, commands) do element
+    translate(scale(element, (1/7, -1/7)), (10, 150))
 end
 window1 = Windows.Window(items=[canvas], title="freetype2", frame=(x=10, y=10, width=width-20, height=height-20))
 closenotify = Condition()
